83d3e7ccfe57fbf6630140f94e4a14d75f40883c
Riyas Abdulkalam <riyaskalam09@gmail.com>
2021-04-28 13:56:50 +0000

    Initial Commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get('/',(req,res) => {
+	res.send('Hello, Bugaboo!');
+});
+
+app.listen(3000, () => console.log("Example app listening on port 3000!"));
+
+
+
