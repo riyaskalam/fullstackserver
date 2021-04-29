112f2e556b3606516db228dbeb8b7c71fa4860df
Riyas Abdulkalam <riyaskalam09@gmail.com>
2021-04-29 14:03:19 +0200

    Add a path to return 418 status code and set custom header

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get('/',(req,res) => {
-	res.send('Hello, Bugaboo!');
+app.get("/", (req, res) => {
+  res.send("Hello, Bugaboo!");
+});
+
+app.get("/home", (req, res) => {
+  res.status(418);
+  res.set({ "X-Full-Stack": "Jem Young is great" });
 });
 
 app.listen(3000, () => console.log("Example app listening on port 3000!"));
-
-
-
