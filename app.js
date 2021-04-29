15314766eedb604bc8acea4b975dcdf5c2ef69d7
Riyas Abdulkalam <riyaskalam09@gmail.com>
2021-04-29 14:12:14 +0200

    Added a response

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello, Bugaboo!");
+  res.send("Hello, Rimara!");
 });
 
 app.get("/home", (req, res) => {
+  res.set("X-Full-Stack", "Jem Young is great");
   res.status(418);
-  res.set({ "X-Full-Stack": "Jem Young is great" });
+  res.send("I prefer coffee");
 });
 
 app.listen(3000, () => console.log("Example app listening on port 3000!"));
